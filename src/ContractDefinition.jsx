c0730c25cce588809a1ef0d41931d2a379a098cb
Willian Mitsuda <wmitsuda@gmail.com>
2019-04-06 04:41:13 -0300

    Extracted drawer component

--- src/ContractDefinition.jsx
+++ src/ContractDefinition.jsx
@@ -1,24 +1,15 @@
 import React, { useState, useEffect, useMemo } from "react";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListSubheader from "@material-ui/core/ListSubheader";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import AlphaCcircleOutline from "mdi-material-ui/AlphaCcircleOutline";
-import AlphaScircleOutline from "mdi-material-ui/AlphaScircleOutline";
 import { Identicon } from "ethereum-react-components";
 import styled from "styled-components";
 import { useWeb3 } from "./Web3Context";
-import { useDrawer } from "./DrawerContext";
 import AnchorLink from "./AnchorLink";
 import FunctionDefinition from "./FunctionDefinition";
+import ContractDrawer from "./ContractDrawer";
 
 const ContractDefinition = ({ address, abi }) => {
   const [contract, setContract] = useState();
@@ -27,7 +18,6 @@ const ContractDefinition = ({ address, abi }) => {
   ]);
   const abiEvents = useMemo(() => abi.filter(f => f.type === "event"), [abi]);
   const web3 = useWeb3();
-  const { isDrawerOpen, closeDrawer } = useDrawer();
 
   useEffect(() => {
     const createContract = async () => {
@@ -42,28 +32,9 @@ const ContractDefinition = ({ address, abi }) => {
 
   return (
     <ContractSection>
-      <AnchorLink id="_contract" />
       <Grid direction="column" spacing={16} container>
         <Grid item>
-          <Card>
-            <CardHeader
-              avatar={<Identicon address={address} size="small" />}
-              title={
-                <Grid container alignItems="center" spacing={8}>
-                  <Grid item>
-                    <Typography variant="h6" inline={true}>
-                      {address}
-                    </Typography>
-                  </Grid>
-                  <Grid item>
-                    <Button variant="outlined" color="secondary">
-                      Change...
-                    </Button>
-                  </Grid>
-                </Grid>
-              }
-            />
-          </Card>
+          <OverviewCard address={address} />
         </Grid>
         {contract &&
           abiFunctions.map((f, key) => (
@@ -77,43 +48,7 @@ const ContractDefinition = ({ address, abi }) => {
             </Grid>
           ))}
       </Grid>
-      <Drawer anchor="left" open={isDrawerOpen} onClose={closeDrawer}>
-        <List subheader={<ListSubheader>Overview</ListSubheader>}>
-          <ListItem
-            component="a"
-            href="#_contract"
-            onClick={closeDrawer}
-            button
-          >
-            <ListItemIcon>
-              <Identicon address={address} size="small" />
-            </ListItemIcon>
-            <ListItemText primary={address} />
-          </ListItem>
-          <Divider />
-        </List>
-        <List subheader={<ListSubheader>Functions</ListSubheader>}>
-          {contract &&
-            abiFunctions.map((f, key) => (
-              <ListItem
-                key={key}
-                component="a"
-                href={`#${f.name}`}
-                onClick={closeDrawer}
-                button
-              >
-                <ListItemIcon>
-                  {f.constant ? (
-                    <AlphaCcircleOutline />
-                  ) : (
-                    <AlphaScircleOutline />
-                  )}
-                </ListItemIcon>
-                <ListItemText primary={f.name} />
-              </ListItem>
-            ))}
-        </List>
-      </Drawer>
+      <ContractDrawer address={address} abiFunctions={abiFunctions} />
     </ContractSection>
   );
 };
@@ -122,5 +57,28 @@ const ContractSection = styled.section`
   background-color: #eeeeee;
   padding: 2rem;
 `;
+
+const OverviewCard = ({ address }) => (
+  <Card>
+    <AnchorLink id="_contract" />
+    <CardHeader
+      avatar={<Identicon address={address} size="small" />}
+      title={<OverviewCardTitle address={address} />}
+    />
+  </Card>
+);
+
+const OverviewCardTitle = ({ address }) => (
+  <Grid container alignItems="center" spacing={8}>
+    <Grid item>
+      <Typography variant="h6">{address}</Typography>
+    </Grid>
+    <Grid item>
+      <Button variant="outlined" color="secondary">
+        Change...
+      </Button>
+    </Grid>
+  </Grid>
+);
 
 export default ContractDefinition;
---
+++ src/ContractDrawer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import { Identicon } from "ethereum-react-components";
+import AlphaCcircleOutline from "mdi-material-ui/AlphaCcircleOutline";
+import AlphaScircleOutline from "mdi-material-ui/AlphaScircleOutline";
+import { useDrawer } from "./DrawerContext";
+
+const ContractDrawer = ({ address, abiFunctions }) => {
+  const { isDrawerOpen, closeDrawer } = useDrawer();
+
+  return (
+    <Drawer anchor="left" open={isDrawerOpen} onClose={closeDrawer}>
+      <OverviewSection address={address} closeDrawer={closeDrawer} />
+      <List subheader={<ListSubheader>Functions</ListSubheader>}>
+        {abiFunctions.map((f, key) => (
+          <GoToFunctionItem key={key} f={f} closeDrawer={closeDrawer} />
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+const OverviewSection = ({ address, closeDrawer }) => (
+  <List subheader={<ListSubheader>Overview</ListSubheader>}>
+    <ListItem component="a" href="#_contract" onClick={closeDrawer} button>
+      <ListItemIcon>
+        <Identicon address={address} size="small" />
+      </ListItemIcon>
+      <ListItemText primary={address} />
+    </ListItem>
+    <Divider />
+  </List>
+);
+
+const GoToFunctionItem = ({ f, closeDrawer }) => (
+  <ListItem component="a" href={`#${f.name}`} onClick={closeDrawer} button>
+    <ListItemIcon>
+      {f.constant ? <AlphaCcircleOutline /> : <AlphaScircleOutline />}
+    </ListItemIcon>
+    <ListItemText primary={f.name} />
+  </ListItem>
+);
+
+export default ContractDrawer;
