2ef6b407abf30636e36943117020f402f56d8c47
Willian Mitsuda <wmitsuda@gmail.com>
2019-04-08 02:12:24 -0300

    Added dynamic title

--- src/ContractPage.jsx
+++ src/ContractPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { DrawerContext } from "./DrawerContext";
 import GlobalAppBar from "./GlobalAppBar";
@@ -7,6 +7,10 @@ import ContractDefinition from "./ContractDefinition";
 const ContractPage = ({ match, abi, abiSetter }) => {
   const { contractAddress } = match.params;
   const [isDrawerOpen, setDrawerOpen] = useState(false);
+
+  useEffect(() => {
+    document.title = `Smart Contract Inspector | ${contractAddress}`;
+  }, [contractAddress]);
 
   return (
     <main>
